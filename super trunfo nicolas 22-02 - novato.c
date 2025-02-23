d0ba688716cf31940ef7aaad2c910fa2a5a4707c
nicolasbare <nicolas.bare@gmail.com>
2025-02-22 23:17:03 -0300

    Add files via upload

---
+++ super trunfo nicolas 22-02 - novato.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+
+    char A, B;
+    char A1[3], B2[3];
+    char SaoPaulo[30], Parana[30];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
+
+//carta A1
+
+    printf("Dados da carta: \n");
+    scanf("%c", &A);
+    printf("entre com o codigo: \n");
+    scanf("%s", A1);
+    printf("entre com nome da cidade: \n");
+    scanf("%s", SaoPaulo);
+    printf("entre com a populacao: \n");
+    scanf("%d", &populacao1);
+    printf("entre com a area: \n");
+    scanf("%f", &area1);
+    printf("entre com o pib: \n");
+    scanf("%f", &pib1);
+    printf("entre com pontosTuristicos: \n");
+    scanf("%d", &pontosTuristicos1);
+
+// carta B2
+
+    printf("Dados da carta: \n");
+    scanf("%c", &B);
+    printf("entre com o codigo: \n");
+    scanf("%s", B2);
+    printf("entre com o nome da cidade: \n");
+    scanf("%s", Parana);
+    printf("entre com a populacao: \n");
+    scanf("%d", &populacao2);
+    printf("entre com a area: \n");
+    scanf("%f", &area2);
+    printf("entre com o pib: \n");
+    scanf ("%f", &pib2);
+    printf("entre com pontosTuristicos: \n");
+    scanf("%d", &pontosTuristicos2);
+   
+
+
+return 0;
+ }
+        /* code */
+    
+    
+
+
+
+
+
+
+
+
